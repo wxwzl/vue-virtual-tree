8f5966f4bc25ddc228f94847184ecbc7d67890bc
wxwzl <zgjc456@163.com>
2025-11-14 22:45:51 +0800

    feat: 产物类型声明生成

--- packages/vue-virtual-tree/vite.config.ts
+++ packages/vue-virtual-tree/vite.config.ts
@@ -1,17 +1,25 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
+import dts from 'vite-plugin-dts'
 
 export default defineConfig({
   server: {
     port: 3000,
   },
-  plugins: [vue()],
+  plugins: [vue(),dts({
+    tsconfigPath: './tsconfig.json',
+    outDir: 'dist/types',
+    entryRoot: resolve(__dirname, 'src'),
+    insertTypesEntry: true, // 生成类型声明入口
+    copyDtsFiles: true, // 将生成的类型声明文件复制到输出目录
+    include: ['./src/**/*.vue', './src/**/*.ts', './src/vite-env.d.ts'], // 包含的文件，根据你的项目调整
+  }),],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
       name: 'VueVirtualTree',
-      fileName: (format) => `vue-virtual-tree.${format === 'es' ? 'js' : 'cjs'}`,
+      fileName: (format) => `index.${format === 'es' ? 'js' : 'cjs'}`,
       formats: ['es', 'cjs']
     },
     rollupOptions: {
@@ -25,12 +33,7 @@ export default defineConfig({
     },
     cssCodeSplit: false,
     sourcemap: true,
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true
-      }
-    }
+    minify: 'esbuild'
   },
   resolve: {
     alias: {
