505ead30f0fb64aec53a30d494fd1f83f0a81554
wxwzl <zgjc456@163.com>
2026-08-01 17:11:58 +0800

    fix(scroller): temporarily remove VirtualScroller import until component is implemented

--- packages/vue-virtual-scroller/src/index.ts
+++ packages/vue-virtual-scroller/src/index.ts
@@ -1,10 +1,3 @@
-import VirtualScroller from "./components/VirtualScroller.vue";
-
-export { VirtualScroller };
 export type { VirtualScrollerProps, VirtualScrollerExpose } from "./types";
 
-export default {
-  install(app: any) {
-    app.component("VirtualScroller", VirtualScroller);
-  },
-};
+// VirtualScroller component will be exported here after Task 3
